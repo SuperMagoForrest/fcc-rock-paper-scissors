e8913ace26f2549c01d29445a8e87c1a33d217a8
superMagoForrest <117987511+SuperMagoForrest@users.noreply.github.com>
2025-09-22 15:02:01 +0200

    Create test_module.py

---
+++ test_module.py
@@ -0,0 +1,24 @@
+# test_module.py
+import unittest
+from RPS_game import play, quincy, abbey, kris, mrugesh
+from RPS import player
+
+class RPS_Test(unittest.TestCase):
+    def test_quincy(self):
+        result = play(player, quincy, 1000)
+        self.assertGreater(result["Player 1"], 600)
+
+    def test_abbey(self):
+        result = play(player, abbey, 1000)
+        self.assertGreater(result["Player 1"], 600)
+
+    def test_kris(self):
+        result = play(player, kris, 1000)
+        self.assertGreater(result["Player 1"], 600)
+
+    def test_mrugesh(self):
+        result = play(player, mrugesh, 1000)
+        self.assertGreater(result["Player 1"], 600)
+
+if __name__ == "__main__":
+    unittest.main()
